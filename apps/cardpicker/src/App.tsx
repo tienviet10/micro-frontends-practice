1a3f49ece1f3b8ed86ffe100270131bca1a6a321
tienviet10 <viettran101294@gmail.com>
2023-07-02 17:21:52 -0400

    Set up micro-frontends

---
+++ apps/cardpicker/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Shell } from "ui";
+
+function App() {
+  return (
+    <Shell title="Card Picker">
+      <div>Card picker woohoo! </div>
+    </Shell>
+  );
+}
+
+export default App;
